770dd717da748e1b4ef7b64489ba65bed601d56a
zubayer <zubayer@ZUBAYER>
2021-04-11 12:01:34 +0600

    rearrange codes

--- src/main/java/com/asl/enums/TransactionCodeType.java
+++ src/main/java/com/asl/enums/TransactionCodeType.java
@@ -9,34 +9,43 @@ import com.asl.model.TransactionCodes;
  */
 public enum TransactionCodeType {
 
-	SUPPLIER_NUMBER("Supplier Number", "SU-", TransactionCodes.SUPPLIER_NUMBER),
-	CUSTOMER_NUMBER("Customer Number", "CUS-", TransactionCodes.CUSTOMER_NUMBER),
-	PURCHASE_ORDER("Purchase Order", "PO--", TransactionCodes.PURCHASE_ORDER),
-	REQUISITION_ORDER("Requisition Order", "REQ-", TransactionCodes.REQUISITION_ORDER),
-	ITEM_NUMBER("Item Number", "IC-", TransactionCodes.ITEM_NUMBER),
-	GRN_NUMBER("GRN Number", "GRN-", TransactionCodes.GRN_NUMBER),
-	PRN_NUMBER("PRN Number", "PR--", null),
-	INVENTORY_NUMBER("Inventory Number", "IMT-", TransactionCodes.INVENTORY_NUMBER),
-	INVENTORY_TRANSACTION("Inventory Transaction", "PO--", TransactionCodes.INVENTORY_TRANSACTION),
-	STOCK_TAKE("Stock Take", "TAG-", null),
-	SALES_ORDER("Sales Order", "SO-", TransactionCodes.SALES_ORDER),
-	CHALAN_NUMBER("Chalan Number", "CH-", TransactionCodes.CHALAN_NUMBER),
-	VOUCHER_NUMBER("Voucher Number", "VCH-",null),
 	ACCOUNT_PAYABLE("Account Payable", "AP-", TransactionCodes.AP_TRANSACTION),
 	ACCOUNT_PAYMENT("Account Payment", "PM-", TransactionCodes.AP_TRANSACTION),
 	ACCOUNT_OBAP("Supplier Opening Entry", "OBAP-", TransactionCodes.AP_TRANSACTION),
 	ACCOUNT_ADAP("Supplier Adjustment", "ADAP-", TransactionCodes.AP_TRANSACTION),
 	ACCOUNT_ADAR("Customer Adjustment", "ADAR-", TransactionCodes.AR_TRANSACTION),
 	ACCOUNT_MR("Money Receipt", "MR-", TransactionCodes.AR_TRANSACTION),
-	INVENTORY_TRANSFER_ORDER("Stock Transfer Order", "TO-", TransactionCodes.INVENTORY_TRANSFER_ORDER),
+
 	BOM_NUMBER("BOM Number", "BM--", TransactionCodes.BOM_NUMBER),
 	BATCH_NUMBER("Batch Number", "BN--", null),
+
+	CUSTOMER_NUMBER("Customer Number", "CUS-", TransactionCodes.CUSTOMER_NUMBER),
+	CHALAN_NUMBER("Chalan Number", "CH-", TransactionCodes.CHALAN_NUMBER),
+
+	GRN_NUMBER("GRN Number", "GRN-", TransactionCodes.GRN_NUMBER),
+
+	ITEM_NUMBER("Item Number", "IC-", TransactionCodes.ITEM_NUMBER),
+	INVENTORY_NUMBER("Inventory Number", "IMT-", TransactionCodes.INVENTORY_NUMBER),
+	INVENTORY_TRANSACTION("Inventory Transaction", "PO--", TransactionCodes.INVENTORY_TRANSACTION),
+	INVENTORY_TRANSFER_ORDER("Stock Transfer Order", "TO-", TransactionCodes.INVENTORY_TRANSFER_ORDER),
+
+	PURCHASE_ORDER("Purchase Order", "PO--", TransactionCodes.PURCHASE_ORDER),
+	PRN_NUMBER("PRN Number", "PR--", null),
 	PROC_ERROR("PROC Error", "ERROR", null),
+	PROFILE_LINE("Profile Line", "PRL-", null),
+	PROFILE_ALLOCATION("Profile Allocation", "PAL-", null),
+
+	REQUISITION_ORDER("Requisition Order", "REQ-", TransactionCodes.REQUISITION_ORDER),
+
+	SUPPLIER_NUMBER("Supplier Number", "SU-", TransactionCodes.SUPPLIER_NUMBER),
+	STOCK_TAKE("Stock Take", "TAG-", null),
+	SALES_ORDER("Sales Order", "SO-", TransactionCodes.SALES_ORDER),
 	SALES_AND_INVOICE_NUMBER("Sales & Invoice Number", "DO--", null),
 	SRN_RETURN("Sales Return", "SR--", null),
+
 	TRANSACTION_TRANSFER("Transfer Transaction", "PROD", null),
-	PROFILE_LINE("Profile Line", "PRL-", null),
-	PROFILE_ALLOCATION("Profile Allocation", "PAL-", null);
+
+	VOUCHER_NUMBER("Voucher Number", "VCH-",null);
 
 	private String code;
 	private String defaultCode;
