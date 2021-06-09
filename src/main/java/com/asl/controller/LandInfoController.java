4352e28b0ad751b0696d0189fbcfff039cc53860
asl <asl@DESKTOP-UIRBOSC>
2021-06-09 19:01:23 +0600

    09-06-2021

--- src/main/java/com/asl/controller/LandInfoController.java
+++ src/main/java/com/asl/controller/LandInfoController.java
@@ -1,5 +1,7 @@
 package com.asl.controller;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Map;
 
 import org.apache.commons.lang3.StringUtils;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.asl.entity.LandInfo;
 import com.asl.entity.LandPerson;
+import com.asl.entity.LandSurveyor;
 import com.asl.enums.CodeType;
 import com.asl.enums.ResponseStatus;
 import com.asl.enums.TransactionCodeType;
@@ -35,6 +38,7 @@ public class LandInfoController extends ASLAbstractController {
 		model.addAttribute("allInfos", landInfoService.getAllLandInfo());
 		model.addAttribute("prefixes", xtrnService.findByXtypetrn(TransactionCodeType.LAND_ID.getCode(), Boolean.TRUE));
 		model.addAttribute("statusTypes", xcodesService.findByXtype(CodeType.STATUS_TYPE.getCode(), Boolean.TRUE));
+		model.addAttribute("landUnitTypes", xcodesService.findByXtype(CodeType.LAND_UNIT.getCode(), Boolean.TRUE));
 		return "pages/land/landinfo";
 	}
 	
@@ -42,6 +46,17 @@ public class LandInfoController extends ASLAbstractController {
 		LandInfo lf  = new LandInfo();
 		lf.setXtypetrn(TransactionCodeType.LAND_ID.getCode());
 		lf.setXtrn(TransactionCodeType.LAND_ID.getdefaultCode());
+		lf.setXcitylanddag(0);
+		lf.setXcitylandqty(BigDecimal.ZERO.setScale(2, RoundingMode.DOWN));
+		lf.setXcslanddag(0);
+		lf.setXcslandqty(BigDecimal.ZERO.setScale(2, RoundingMode.DOWN));
+		lf.setXlanddedother(0);
+		lf.setXlanddedroad(0);
+		lf.setXrslanddag(0);
+		lf.setXrslandqty(BigDecimal.ZERO.setScale(2, RoundingMode.DOWN));
+		lf.setXriversideqty(BigDecimal.ZERO.setScale(2, RoundingMode.DOWN));
+		
+		
 		return lf;
 	}
 	
@@ -50,11 +65,12 @@ public class LandInfoController extends ASLAbstractController {
 		LandInfo landInfo = landInfoService.findByLandInfo(xland);
 		if (landInfo == null) return "redirect:/landinfo";
 
-		
+
 		model.addAttribute("info", landInfo);
 		model.addAttribute("allInfos", landInfoService.getAllLandInfo());
 		model.addAttribute("prefixes", xtrnService.findByXtypetrn(TransactionCodeType.LAND_ID.getCode(), Boolean.TRUE));
 		model.addAttribute("statusTypes", xcodesService.findByXtype(CodeType.STATUS_TYPE.getCode(), Boolean.TRUE));
+		model.addAttribute("landUnitTypes", xcodesService.findByXtype(CodeType.LAND_UNIT.getCode(), Boolean.TRUE));
 		return "pages/land/landinfo";
 	}
 	
@@ -89,6 +105,35 @@ public class LandInfoController extends ASLAbstractController {
 		responseHelper.setRedirectUrl("/landinfo/" + landInfo.getXland());
 		return responseHelper.getResponse();
 	}
+	
+	@PostMapping("/archive/{xland}")
+	public @ResponseBody Map<String, Object> archive(@PathVariable String xland){
+		return doArchiveOrRestore(xland, true);
+	}
 
+	@PostMapping("/restore/{xland}")
+	public @ResponseBody Map<String, Object> restore(@PathVariable String xland){
+		return doArchiveOrRestore(xland, false);
+	}
+
+	public Map<String, Object> doArchiveOrRestore(String xland, boolean archive){
+		LandInfo lp = landInfoService.findByLandInfo(xland);
+		if(lp == null) {
+			responseHelper.setStatus(ResponseStatus.ERROR);
+			return responseHelper.getResponse();
+		}
+
+		lp.setZactive(archive ? Boolean.FALSE : Boolean.TRUE);
+		long count = landInfoService.update(lp);
+		if(count == 0) {
+			responseHelper.setStatus(ResponseStatus.ERROR);
+			return responseHelper.getResponse();
+		}
+
+		responseHelper.setSuccessStatusAndMessage("Land Information updated successfully");
+		responseHelper.setRedirectUrl("/landinfo/" + lp.getXland());
+		return responseHelper.getResponse();
+	}
 
 }
+	
--- src/main/java/com/asl/controller/LandSurveyorController.java
+++ src/main/java/com/asl/controller/LandSurveyorController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.asl.entity.LandPerson;
 import com.asl.entity.LandSurveyor;
 import com.asl.enums.ResponseStatus;
 import com.asl.enums.TransactionCodeType;
@@ -67,21 +68,52 @@ public class LandSurveyorController extends ASLAbstractController{
 			BeanUtils.copyProperties(landSurveyor, xlp,"xtypetrn","xtrn");
 			long count = landSurveyorService.update(xlp);
 			if(count == 0) {
-				responseHelper.setErrorStatusAndMessage("Can't update land info");
+				responseHelper.setErrorStatusAndMessage("Can't update surveyor info");
 				return responseHelper.getResponse();
 			}
-			responseHelper.setSuccessStatusAndMessage("Land info updated successfully");
+			responseHelper.setSuccessStatusAndMessage("Surveyor info updated successfully");
 			responseHelper.setRedirectUrl("/landsurveyor/" + xlp.getXsurveyor());
 			return responseHelper.getResponse();
 		}
 		// if new
-				long count = landSurveyorService.save(landSurveyor);
-				if(count == 0) {
-					responseHelper.setErrorStatusAndMessage("Can't save surveyor info");
-					return responseHelper.getResponse();
-				}
-				responseHelper.setSuccessStatusAndMessage("Surveyor info saved successfully");
-				responseHelper.setRedirectUrl("/landsurveyor/" + landSurveyor.getXsurveyor());
-				return responseHelper.getResponse();
-			}
+		long count = landSurveyorService.save(landSurveyor);
+		if(count == 0) {
+			responseHelper.setErrorStatusAndMessage("Can't save surveyor info");
+			return responseHelper.getResponse();
+		}
+		responseHelper.setSuccessStatusAndMessage("Surveyor info saved successfully");
+		responseHelper.setRedirectUrl("/landsurveyor/" + landSurveyor.getXsurveyor());
+			return responseHelper.getResponse();
+		}
+	
+	@PostMapping("/archive/{xsurveyor}")
+	public @ResponseBody Map<String, Object> archive(@PathVariable String xsurveyor){
+		return doArchiveOrRestore(xsurveyor, true);
+	}
+
+	@PostMapping("/restore/{xsurveyor}")
+	public @ResponseBody Map<String, Object> restore(@PathVariable String xsurveyor){
+		return doArchiveOrRestore(xsurveyor, false);
+	}
+
+	public Map<String, Object> doArchiveOrRestore(String xsurveyor, boolean archive){
+		LandSurveyor lp = landSurveyorService.findByLandSurveyor(xsurveyor);
+		if(lp == null) {
+			responseHelper.setStatus(ResponseStatus.ERROR);
+			return responseHelper.getResponse();
+		}
+
+		lp.setZactive(archive ? Boolean.FALSE : Boolean.TRUE);
+		long count = landSurveyorService.update(lp);
+		if(count == 0) {
+			responseHelper.setStatus(ResponseStatus.ERROR);
+			return responseHelper.getResponse();
+		}
+
+		responseHelper.setSuccessStatusAndMessage("Surveyor Information updated successfully");
+		responseHelper.setRedirectUrl("/landsurveyor/" + lp.getXsurveyor());
+		return responseHelper.getResponse();
+	}
+
+	
 }
